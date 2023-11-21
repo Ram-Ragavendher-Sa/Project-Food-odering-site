3578cec8c562d4b6acfc7dd18d96efe1b3eb7c8a
Ram-Ragavendher-Sa <ragavendherram@gmail.com>
2023-11-21 12:18:14 +0530

    final review 0

--- src/Testing/Desc.jsx
+++ src/Testing/Desc.jsx
@@ -7,9 +7,9 @@ const Desc = () => {
     <div style={{display: 'flex',flexDirection: 'row',justifyContent:'space-evenly',margin:'20px'}}>
         <Container>
             <div>
-                <h1>Way the food Reaches You....</h1>
+                <h1 style={{color:'white'}}>Way the food Reaches You....</h1>
                 <br></br>
-                <div>
+                <div style={{color:'white'}}>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptates reiciendis architecto? Fugit tempore exercitationem optio dolore animi? Excepturi deserunt odio, architecto facere eius quod totam voluptatum quis reiciendis cum.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quos sed eum nemo vero eveniet optio repellendus nobis deleniti ab ut, alias consequuntur a illum dolorem voluptate, quo, laudantium recusandae.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consequatur deleniti pariatur quidem voluptate eveniet dolorum, quibusdam vitae non. Consequatur fugiat delectus ad illo est, tempore magni asperiores ab vel?
@@ -25,8 +25,8 @@ const Desc = () => {
             <img src="https://media-cdn.tripadvisor.com/media/photo-s/05/7d/97/20/tandoori-chicken-sizzler.jpg" alt="img" style={{height:'50vh',width:'40vw'}}/>
         </Container>
         <Container>
-            <div>
-                <h1>Serving you Hot....</h1>
+            <div style={{color:'white'}}>
+                <h1 >Serving you Hot....</h1>
                 <br></br>
                 <div>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptates reiciendis architecto? Fugit tempore exercitationem optio dolore animi? Excepturi deserunt odio, architecto facere eius quod totam voluptatum quis reiciendis cum.
