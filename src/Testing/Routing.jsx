3124139f5dc01ba28f2a9ee624c01f1ce1d0170f
Ram-Ragavendher-Sa <ragavendherram@gmail.com>
2023-11-27 09:44:23 +0530

    b

--- src/Testing/Routing.jsx
+++ src/Testing/Routing.jsx
@@ -8,6 +8,7 @@ import Res1 from "./Restaurent1";
 import Res2 from "./Restaurent2";
 import Res3 from "./Restaurent3";
 import Res4 from "./Restaurent4";
+
 const router=createBrowserRouter([
     { path:"/", element: <Homeimgfile/>},
     { path:"/lop",element: <Lope/>},
